88b10e49b71682f9cad77a73a5bec21f1447807b
Mark Collin <mark@ardescosolutions.com>
2012-01-09 16:57:29 +0000

    Added TODO to jMeterCommandLineArguments to keep track of what needs to be added.

--- src/main/java/org/apache/jmeter/JMeterCommandLineArguments.java
+++ src/main/java/org/apache/jmeter/JMeterCommandLineArguments.java
@@ -10,26 +10,26 @@ public enum JMeterCommandLineArguments {
     PROXY_PASSWORD("a"),
     JMETER_HOME_OPT("d"),
     HELP_OPT("h"),              //Shows help information, never used in this plugin.
-    JMLOGFILE_OPT("j"),
+    JMLOGFILE_OPT("j"),         //TODO
     LOGFILE_OPT("l"),
     NONGUI_OPT("n"),
     PROPFILE_OPT("p"),
     PROPFILE2_OPT("q"),
     REMOTE_OPT("r"),
-    SERVER_OPT("s"),
+    SERVER_OPT("s"),            //TODO
     TESTFILE_OPT("t"),
     PROXY_USERNAME("u"),
-    VERSION_OPT("v"),
+    VERSION_OPT("v"),           //TODO
     SYSTEM_PROPERTY("D"),
     JMETER_GLOBAL_PROP("G"),
     PROXY_HOST("H"),
     JMETER_PROPERTY("J"),
     LOGLEVEL("L"),
-    NONPROXY_HOSTS("N"),
+    NONPROXY_HOSTS("N"),        //TODO
     PROXY_PORT("P"),
-    REMOTE_OPT_PARAM("R"),
-    SYSTEM_PROPFILE("S"),
-    REMOTE_STOP("X");
+    REMOTE_OPT_PARAM("R"),      //TODO
+    SYSTEM_PROPFILE("S"),       //TODO
+    REMOTE_STOP("X");           //TODO
 
     private final String commandLineArgument;
 

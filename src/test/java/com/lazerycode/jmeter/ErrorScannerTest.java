4b543c1bb2bb1a108d994f016d804cd0658c4ed7
Arne Franken <arne.franken@coremedia.com>
2012-01-30 11:45:32 +0100

    remove "org.hamcrest" since plain junit is much more readable

--- src/test/java/com/lazerycode/jmeter/ErrorScannerTest.java
+++ src/test/java/com/lazerycode/jmeter/ErrorScannerTest.java
@@ -1,56 +1,46 @@
 package com.lazerycode.jmeter;
 
-import com.lazerycode.jmeter.ErrorScanner;
-import org.junit.Test;
-
+import junit.framework.TestCase;
 import java.io.File;
 import java.net.URI;
 
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+public class ErrorScannerTest extends TestCase {
 
-public class ErrorScannerTest {
-
-    @Test
     public void testLineContainsForErrors() {
         ErrorScanner scanner = new ErrorScanner(false, false);
-        assertThat(scanner.lineContainsForErrors("<failure>false</failure>"), is(equalTo(false)));
-        assertThat(scanner.lineContainsForErrors("<failure>true</failure>"), is(equalTo(true)));
-        assertThat(scanner.lineContainsForErrors("<error>true</error>"), is(equalTo(true)));
-        assertThat(scanner.lineContainsForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"false\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "), is(equalTo(true)));
-        assertThat(scanner.lineContainsForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"true\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "), is(equalTo(false)));
-        assertThat(scanner.getFailureCount(), is(equalTo(2)));
-        assertThat(scanner.getErrorCount(), is(equalTo(1)));
+        assertFalse(scanner.checkLineForErrors("<failure>false</failure>"));
+        assertTrue(scanner.checkLineForErrors("<failure>true</failure>"));
+        assertTrue(scanner.checkLineForErrors("<error>true</error>"));
+        assertTrue(scanner.checkLineForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"false\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "));
+        assertFalse(scanner.checkLineForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"true\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "));
+        assertEquals(scanner.getFailureCount(), 2);
+        assertEquals(scanner.getErrorCount(), 1);
     }
 
-    @Test
     public void testLineContainsWhenIgnoringErrorsAndFailures() {
         ErrorScanner scanner = new ErrorScanner(true, true);
-        assertThat(scanner.lineContainsForErrors("<failure>false</failure>"), is(equalTo(false)));
-        assertThat(scanner.lineContainsForErrors("<failure>true</failure>"), is(equalTo(false)));
-        assertThat(scanner.lineContainsForErrors("<error>true</error>"), is(equalTo(false)));
-        assertThat(scanner.lineContainsForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"false\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "), is(equalTo(false)));
-        assertThat(scanner.lineContainsForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"true\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "), is(equalTo(false)));
-        assertThat(scanner.getFailureCount(), is(equalTo(0)));
-        assertThat(scanner.getErrorCount(), is(equalTo(0)));
+        assertFalse(scanner.checkLineForErrors("<failure>false</failure>"));
+        assertFalse(scanner.checkLineForErrors("<failure>true</failure>"));
+        assertFalse(scanner.checkLineForErrors("<error>true</error>"));
+        assertFalse(scanner.checkLineForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"false\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "));
+        assertFalse(scanner.checkLineForErrors("<httpSample t=\"44\" lt=\"44\" ts=\"1303959710655\" s=\"true\" lb=\"/energy/mets/day/{endDate}/{StartDate} Reversed Dates\" rc=\"400\" rm=\"Bad Request\" "));
+        assertEquals(scanner.getFailureCount(), 0);
+        assertEquals(scanner.getErrorCount(), 0);
     }
 
-    @Test
-    public void jtlFormatFileWithFailures() throws Exception {
+    public void testJtlFormatFileWithFailures() throws Exception {
         ErrorScanner scanner = new ErrorScanner(false, false);
         URI testResultsFile = this.getClass().getResource("/jtl2-1-fail.jtl").toURI();
-        assertThat(scanner.hasTestPassed(new File(testResultsFile)), is(equalTo(false)));
-        assertThat(scanner.getFailureCount(), is(equalTo(2)));
-        assertThat(scanner.getErrorCount(), is(equalTo(0)));
+        assertFalse(scanner.hasTestPassed(new File(testResultsFile)));
+        assertEquals(scanner.getFailureCount(), 2);
+        assertEquals(scanner.getErrorCount(), 0);
     }
 
-    @Test
-    public void jtlFormatFileWithNoFailures() throws Exception {
+    public void testJtlFormatFileWithNoFailures() throws Exception {
         ErrorScanner scanner = new ErrorScanner(false, false);
         URI testResultsFile = this.getClass().getResource("/jtl2-1-pass.jtl").toURI();
-        assertThat(scanner.hasTestPassed(new File(testResultsFile)), is(equalTo(true)));
-        assertThat(scanner.getFailureCount(), is(equalTo(0)));
-        assertThat(scanner.getErrorCount(), is(equalTo(0)));
+        assertTrue(scanner.hasTestPassed(new File(testResultsFile)));
+        assertEquals(scanner.getFailureCount(), 0);
+        assertEquals(scanner.getErrorCount(), 0);
     }
 }

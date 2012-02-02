674749b29560955d1ea2808852211c4a224405e2
Mark Collin <mark@ardescosolutions.com>
2012-02-02 23:07:05 +0000

    Removed unused log4j dependency Added maven-plugin-testing-harness Added a JMeterMojo test stub

---
+++ src/test/java/com/lazerycode/jmeter/JMeterMojoTest.java
@@ -0,0 +1,17 @@
+package com.lazerycode.jmeter;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Test;
+
+//TODO write tests for JMeterMojo
+public class JMeterMojoTest extends AbstractMojoTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void test(){
+
+    }
+}

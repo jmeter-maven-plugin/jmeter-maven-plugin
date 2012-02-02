a137ed4a72fb0b047bfe2b90dc5287f8f0b143d9
Mark Collin <mark@ardescosolutions.com>
2012-02-02 08:57:34 +0000

    Fixed test by doing a temp cheeky switch from protected to public

--- src/test/java/com/lazerycode/jmeter/PropertyFileMergerTest.java
+++ src/test/java/com/lazerycode/jmeter/PropertyFileMergerTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
 import java.util.HashMap;
@@ -18,25 +19,20 @@ public class PropertyFileMergerTest extends TestCase {
 
     URL testFile = this.getClass().getResource("/jmeter.properties");
 
-    @Ignore
     @Test
     public void testMergeProperties() throws Exception {
-
-
-
         Map<String,String> customProperties = new HashMap<String, String>();
         customProperties.put("log_level.jmeter.control","INFO");
         customProperties.put("log_level.jmeter","DEBUG");
         
         Properties properties = new Properties();
-        properties.load(new FileInputStream(this.testFile.getFile()));
+        properties.load(new FileInputStream(new File(this.testFile.toURI())));
 
-//        PropertyHandler.mergePropertiesFile(getSourcePropertyFile(propertyFile), this.propertyOutputDirectory, propertyFile.getPropertiesFileName(), masterPropertiesMap.get(propertyFile));
-//        Properties modifiedProperties = PropertyFileMerger.mergeProperties(properties, customProperties);
+        Properties modifiedProperties = PropertyHandler.mergeProperties(properties, customProperties);
 
-//        assertEquals("property was not overwritten","DEBUG",modifiedProperties.get("log_level.jmeter"));
-//        assertEquals("property was not added","INFO",modifiedProperties.get("log_level.jmeter.control"));
-//        assertEquals("property should not differ from file entry","DEBUG",modifiedProperties.get("log_level.jmeter.junit"));
+        assertEquals("property was not overwritten","DEBUG",modifiedProperties.get("log_level.jmeter"));
+        assertEquals("property was not added","INFO",modifiedProperties.get("log_level.jmeter.control"));
+        assertEquals("property should not differ from file entry","DEBUG",modifiedProperties.get("log_level.jmeter.junit"));
     }
 
 }

3b08046ab3400f7ed9179fc026f3f6de8ddc7639
Arne Franken <arne.franken@coremedia.com>
2012-04-27 14:09:33 +0200

    add Linux GUI thread name

--- src/main/java/com/lazerycode/jmeter/threadhandling/JMeterThreads.java
+++ src/main/java/com/lazerycode/jmeter/threadhandling/JMeterThreads.java
@@ -4,12 +4,14 @@ package com.lazerycode.jmeter.threadhandling;
  * Thread names added to this list will be used when scanning JMeterThreads directly after JMeter is called
  * The plugin will then wait for the thread to finish
  * TODO: find out which threadname works for GUI detection on other operating systems
+ * TODO: #GUI_THREAD_LINUX was found on a Mint 12 (Ubuntu) with Gnome desktop. is this universal for Linux?
  */
 public enum JMeterThreads {
 
     STANDARD_JMETER_ENGINE("StandardJMeterEngine", false),
     GUI_THREAD_WINDOWS("AWT-Windows", true),
-    GUI_THREAD_MACOSX("AWT-AppKit", true);
+    GUI_THREAD_MACOSX("AWT-AppKit", true),
+    GUI_THREAD_LINUX("AWT-XAWT", true);
 
     private final Object[] threadData;
 

913261256147317f827594f098053b11223ae6b9
Mark Collin <mark.collin@lazeryattack.com>
2013-03-06 18:22:49 +0000

    Refactored IncludesComparator

--- src/main/java/com/lazerycode/jmeter/IncludesComparator.java
+++ src/main/java/com/lazerycode/jmeter/IncludesComparator.java
@@ -1,9 +1,9 @@
 package com.lazerycode.jmeter;
 
-import org.apache.tools.ant.types.selectors.SelectorUtils;
-
 import java.util.Comparator;
 import java.util.List;
+
+import static org.apache.tools.ant.types.selectors.SelectorUtils.matchPath;
 
 /**
  * Compare filenames based on their order in a list of includes (using default
@@ -21,29 +21,16 @@ public class IncludesComparator implements Comparator<String> {
      * Compares two filenames based on the index of the first include pattern
      * that matches them in the given list. If the index is equal, the actual
      * filename lexicographical order is used to ensure a total order.
+     *
+     * @param s1 String
+     * @param s2 String
+     * @return int
      */
     @Override
     public int compare(String s1, String s2) {
-        final int i1 = includesIndex(s1);
-        final int i2 = includesIndex(s2);
+        final int i1 = includes.indexOf(s1);
+        final int i2 = includes.indexOf(s2);
         return (i1 < i2 ? -1 : (i1 == i2 ? s1.compareTo(s2) : 1));
     }
 
-    private int includesIndex(String filename) {
-        if ((includes == null) || (filename == null)) {
-            return 0;
-        }
-        int index = 0;
-        for (final String include : includes) {
-            if (matches(include, filename.replace('\\', '/'))) {
-                return index;
-            }
-            index++;
-        }
-        return 0;
-    }
-
-    private boolean matches(String pattern, String filename) {
-        return SelectorUtils.matchPath(pattern, filename, true);
-    }
 }

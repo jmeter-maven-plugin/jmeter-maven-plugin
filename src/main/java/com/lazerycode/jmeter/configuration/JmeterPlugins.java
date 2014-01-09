64282ec4bdd24fec8c8a455eb4b6860b94a0faad
Mark Collin <mark.collin@lazeryattack.com>
2014-01-09 10:06:32 +0000

    Fix bug where we were getting incorrect dependency list for explicit dependency check. Add in the ability mark an artifact as a JMeter plugin

---
+++ src/main/java/com/lazerycode/jmeter/configuration/JmeterPlugins.java
@@ -0,0 +1,11 @@
+package com.lazerycode.jmeter.configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyre
+ * Date: 09/01/2014
+ * Time: 08:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class JmeterPlugins {
+}

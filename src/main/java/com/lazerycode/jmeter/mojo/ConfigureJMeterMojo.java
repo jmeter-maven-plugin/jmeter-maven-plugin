d87bd4ce7198e783c419746ee119b9be6324e83f
Mark Collin <mark.collin@lazeryattack.com>
2016-07-04 14:11:29 +0100

    Update joda-time to 2.9.4 Update maven-javadoc-plugin to 2.10.4 Update maven-source-plugin to 3.0.1 Use JMeter 3.0 by default instead of 3.0-SNAPSHOT

--- src/main/java/com/lazerycode/jmeter/mojo/ConfigureJMeterMojo.java
+++ src/main/java/com/lazerycode/jmeter/mojo/ConfigureJMeterMojo.java
@@ -55,7 +55,7 @@ public class ConfigureJMeterMojo extends AbstractJMeterMojo {
 	 * if you change this version number the list of artifacts required to run JMeter may change.
 	 * If this happens you will need to override the &lt;jmeterArtifacts&gt; element.
 	 */
-	@Parameter(defaultValue = "3.0-SNAPSHOT")
+	@Parameter(defaultValue = "3.0")
 	private String jmeterVersion;
 
 	/**

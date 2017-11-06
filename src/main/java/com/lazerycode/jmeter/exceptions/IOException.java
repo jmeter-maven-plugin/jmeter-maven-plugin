220d236c42aff56a0eb09330104cc60ca40c9a3a
pmouawad <p.mouawad@ubik-ingenierie.com>
2017-11-06 23:07:23 +0100

    Add serial version id

--- src/main/java/com/lazerycode/jmeter/exceptions/IOException.java
+++ src/main/java/com/lazerycode/jmeter/exceptions/IOException.java
@@ -3,7 +3,12 @@ package com.lazerycode.jmeter.exceptions;
 import org.apache.maven.plugin.MojoExecutionException;
 
 public class IOException extends MojoExecutionException {
-	public IOException(String message, Throwable cause) {
+	/**
+     * 
+     */
+    private static final long serialVersionUID = -459038205882201474L;
+
+    public IOException(String message, Throwable cause) {
 		super(message, cause);
 	}
 }

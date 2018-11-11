cbb6c7986a1f347da5f190c2b1f57d38426de34a
Balu Praveen <balu.praveen@emishealth.com>
2018-11-11 13:44:58 +0530

    Added feature #265

--- src/main/java/com/lazerycode/jmeter/mojo/CheckResultsMojo.java
+++ src/main/java/com/lazerycode/jmeter/mojo/CheckResultsMojo.java
@@ -1,14 +1,15 @@
 package com.lazerycode.jmeter.mojo;
 
-import com.lazerycode.jmeter.json.TestConfig;
-import com.lazerycode.jmeter.testrunner.ResultScanner;
+import java.io.File;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
+import com.lazerycode.jmeter.json.TestConfig;
+import com.lazerycode.jmeter.testrunner.ResultScanner;
 
 /**
  * Goal that computes successes/failures from CSV or XML results files.<br/>
@@ -38,6 +39,14 @@ public class CheckResultsMojo extends AbstractJMeterMojo {
 	 */
 	@Parameter(defaultValue = "true")
 	protected boolean scanResultsForSuccessfulRequests;
+	
+	/**
+	 * Sets the error rate threshold limit for build to get failed, i.e if its set to 3 then build fails only if
+	 * the % of failed requests are above 3
+	 * defaults to 0
+	 */
+	@Parameter(defaultValue = "0")
+	protected float acceptableErrorsInPercent;
 
 	/**
 	 * Scan JMeter result files for successful, and failed requests/
@@ -64,11 +73,13 @@ public class CheckResultsMojo extends AbstractJMeterMojo {
 			getLog().info(" ");
 			getLog().info("Performance Test Results");
 			getLog().info(" ");
-			getLog().info("Result (.jtl) files scanned:	" + testConfig.getResultsFileLocations().size());
-			getLog().info("Successful requests: 		" + resultScanner.getSuccessCount());
-			getLog().info("Failed requests: 			" + resultScanner.getFailureCount());
+			getLog().info("Result (.jtl) files scanned: " + testConfig.getResultsFileLocations().size());
+			getLog().info("Successful requests:         " + resultScanner.getSuccessCount());
+			getLog().info("Failed requests:             " + resultScanner.getFailureCount());
+			float failurePercent = (float)resultScanner.getFailureCount() / (float)(resultScanner.getSuccessCount() + resultScanner.getFailureCount()) * 100;
+			getLog().info("Failures %:                  " + failurePercent + "% (" + acceptableErrorsInPercent + "% accepted)" );
 			getLog().info(" ");
-			if (!ignoreResultFailures && resultScanner.getFailureCount() > 0) {
+			if (!ignoreResultFailures &&  failurePercent > acceptableErrorsInPercent) {
 				throw new MojoFailureException("Failing build because failed requests have been detected.  JMeter logs are available at: '" + logsDirectory.getAbsolutePath() + "'");
 			}
 		} else {

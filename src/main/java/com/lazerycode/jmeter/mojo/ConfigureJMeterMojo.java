1a47923f693b4c6861dc9c00b3c5a0cfcf15b46d
Harjot Singh <hr744527@dal.ca>
2023-04-03 21:32:22 -0300

    Decompose conditional refactoring

--- src/main/java/com/lazerycode/jmeter/mojo/ConfigureJMeterMojo.java
+++ src/main/java/com/lazerycode/jmeter/mojo/ConfigureJMeterMojo.java
@@ -14,6 +14,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.RequestTrace;
 import org.eclipse.aether.artifact.AbstractArtifact;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
@@ -539,6 +540,7 @@ public class ConfigureJMeterMojo extends AbstractJMeterMojo {
         CollectRequest collectRequest = new CollectRequest();
         collectRequest.setRoot(rootDependency);
         collectRequest.setRepositories(repositoryList);
+
         // In #classpathFilter, we are not actually passing the scope, but the classpath identifier (just using the same enum as for the scope).
         // That is, for example, for a test classpath, dependencies are required with any scope (that is, the TEST filter is the softest)
 
@@ -559,11 +561,13 @@ public class ConfigureJMeterMojo extends AbstractJMeterMojo {
                             return notExcluded;
                         });
         DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, dependencyFilter);
+        CollectRequest dependencyRequestCollect = dependencyRequest.getCollectRequest();
+        RequestTrace dependencyRequestTrace = dependencyRequestCollect.getTrace();
 
         if (getLog().isDebugEnabled()) {
             getLog().debug("Root dependency name: " + rootDependency.toString());
-            if ((dependencyRequest.getCollectRequest() != null) && (dependencyRequest.getCollectRequest().getTrace() != null)) {
-                getLog().debug("Root dependency request trace: " + dependencyRequest.getCollectRequest().getTrace().toString());
+            if ((dependencyRequestCollect != null) && (dependencyRequestTrace != null)) {
+                getLog().debug("Root dependency request trace: " + dependencyRequestTrace.toString());
             }
             getLog().debug("Root dependency exclusions: " + rootDependency.getExclusions());
             getLog().debug(LINE_SEPARATOR);
@@ -575,9 +579,9 @@ public class ConfigureJMeterMojo extends AbstractJMeterMojo {
             for (DependencyNode dependencyNode : artifactDependencyNodes) {
                 if (getLog().isDebugEnabled()) {
                     getLog().debug("Dependency name: " + dependencyNode.toString());
-                    if ((dependencyRequest.getCollectRequest() != null) && (dependencyRequest.getCollectRequest().getTrace() != null)) {
+                    if ((dependencyRequestCollect != null) && (dependencyRequestTrace != null)) {
                         getLog().debug("Dependency request trace: " +
-                                dependencyRequest.getCollectRequest().getTrace().toString());
+                                dependencyRequestTrace.toString());
                     }
                 }
                 Exclusion dummyExclusion = new Exclusion(
